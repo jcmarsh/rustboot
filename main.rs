7055ccd06f1501c0c6563aa522fb258ff0881d11
Marshall, James 459937851 <james.marshall-1@nasa.gov>
2019-08-08 09:47:14 -0400

    Print Hello

--- main.rs
+++ main.rs
@@ -20,6 +20,11 @@ pub enum Color {
     White      = 15,
 }
 
+//enum Option<T> {
+//    None,
+//    Some(T)
+//}
+
 struct IntRange {
     cur: u32,
     max: u32
@@ -61,4 +66,28 @@ fn clear_screen(background: Color) {
 //  #[no_split_stack]
 pub fn main() {
     clear_screen(Color::LightRed);
+    //clear_screen(Color::LightGreen);
+
+    // Say Hello.
+    putchar(10,10,72);
+    putchar(11,10,69);
+    putchar(12,10,76);
+    putchar(13,10,76);
+    putchar(14,10,79);
+}
+
+// Copying code from Julia Evans: https://jvns.ca/blog/2014/03/12/the-rust-os-story/
+pub fn putchar(x: u16, y:u16, c:u8) {
+    let idx : u32 = (y * VGA_WIDTH * 2 + x * 2).into();
+    let fg_color = Color::White;
+    let bg_color = Color::DarkGray;
+
+    unsafe {
+        *((VGA_ADDRESS + idx) as *mut u16) = make_vgaentry(c, fg_color, bg_color);
+    }
+}
+
+pub fn make_vgaentry(c: u8, fg: Color, bg: Color) -> u16 {
+    let color = fg as u16 | ((bg as u16) << 4);
+    return c as u16 | (color << 8);
 }
